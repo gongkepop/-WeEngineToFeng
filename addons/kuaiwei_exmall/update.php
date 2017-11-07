96c960d8771d60b4ada1600ce3a27dea46d50d96
gongke <gongke@noahwm.com>
2017-11-07 17:04:42 +0800

    add template

---
+++ addons/kuaiwei_exmall/update.php
@@ -0,0 +1,5 @@
+<?php
+
+if(!pdo_fieldexists('kuaiwei_exmall_base', 'title')) {
+	pdo_query("ALTER TABLE ".tablename('kuaiwei_exmall_base')." ADD `title` varchar(255) DEFAULT '' COMMENT '商城标题';");
+}
